36cca70b5f78d82a6d29273bb7c976ff15b26836
unknown <dkhen@AMS9-2242.booking.pcln.com>
2014-12-19 17:28:31 +0100

    initial

---
+++ MusicMan/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Console;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicMan
+{
+    class Program
+    {
+        static string MusicDir = @"C:\Music";
+        static void Main(string[] args)
+        {
+            var queries = args[0].ToFileInfo().Lines().Where(t=>t.IsNotNullOrEmpty()).ToList();
+            var results = queries.Select(q=> new QueryResult { Query = q, AlbumDir = FindAlbumDir(q) }).ToList();
+            var playlist = results.SelectMany(t => GetAudioFiles(t.AlbumDir)).ToList();
+            playlist.ForEach(t=>WriteLine(t.FullName));
+            File.WriteAllLines("playlist.m3u", playlist.Select(t => t.FullName));
+        }
+
+        static List<FileInfo> GetAudioFiles(DirectoryInfo dir)
+        {
+            return dir.GetFiles().Where(f => f.Extension == ".mp3" || f.Extension == ".flac").ToList();
+        }
+
+        private static DirectoryInfo FindAlbumDir(string q)
+        {
+            var dirs = MusicDir.ToDirectoryInfo().GetDirectories("*", SearchOption.AllDirectories);
+            var list = dirs.Where(dir => DirMatchesQuery(dir, q)).ToList();
+            if (list.Count == 0)
+            {
+                WriteLine("Can't find album for query: {0}", q);
+                return null;
+            }
+            else if (list.Count > 1)
+            {
+                WriteLine("Multiple matches for query: {0}", q);
+                list.ForEach(t => WriteLine(t));
+            }
+            return list[0];
+        }
+        static bool DirMatchesQuery(DirectoryInfo dir, string q)
+        {
+            var xxx = String.Format("{0} {1}", q, dir.FullName); ;
+            //WriteLine("{0} {1}", q, dir.FullName);
+            
+            if (GetAudioFiles(dir).Count==0)
+                return false;
+            var s = dir.FullName.ToLower();
+            q = q.ToLower();
+            var tokens = q.Split(' ');
+            if (tokens.All(token => s.Contains(token)))
+                return true;
+            return false;
+
+        }
+    }
+
+}
---
+++ MusicMan/QueryResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+namespace MusicMan
+{
+    internal class QueryResult
+    {
+        public DirectoryInfo AlbumDir { get; internal set; }
+        public string Query { get; internal set; }
+    }
+}
